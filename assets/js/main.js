fd2fd9faf83038224711c77d0eed30ed9fdfc856
PacaYvonne <yvonne@pacagen.com>
2026-03-03 21:15:28 -0500

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,7 +120,7 @@ function insertFooter(pageType = 'root') {
             <div class="container">
                 <div class="row">
                     <div class="col-md-12 text-center">
-                        <p>&copy; 2025 Daily Fitness Edit. All rights reserved.</p>
+                        <p>&copy; 2026 Daily Fitness Edit. All rights reserved.</p>
                         <p>
                             <a href="${privacyPath}">Privacy Policy</a> | 
                             <a href="${termsPath}">Terms of Service</a>
